a6fc5c24107dc05f51bcd66091ee11396c6bc457
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2025-08-01 11:03:10 +0530

    chore: added swap faucet

--- src/components/WalletBadge.jsx
+++ src/components/WalletBadge.jsx
@@ -1,15 +1,52 @@
 import { useEffect, useState } from "react";
-import { connectWalletToSite, getWalletAddress } from "../utils/wallet";
+import {
+	connectWalletToSite,
+	getWalletAddress,
+	switchChain,
+} from "../utils/wallet";
 import "./WalletBadge.css";
 import { FaEthereum } from "react-icons/fa";
+import { ERC20_ABI, SEPOLIA_WETH_CA } from "../constants";
+import Web3 from "web3";
+import Loader from "./Loader";
+import Notification from "./Notification";
 
 const WalletBadge = () => {
 	const [address, setaddress] = useState(null);
+	const [swapLoading, setSwapLoading] = useState(false);
+	const [notification, setNotification] = useState(null);
 
 	async function setWalletAddress() {
 		const wa = await getWalletAddress();
 		if (!wa || wa.length < 6) return;
-		setaddress(`${wa.slice(0, 4)}...${wa.slice(-3)}`);
+		setaddress(wa);
+	}
+
+	async function handleSwap() {
+		if (!address) return alert("Connect your wallet");
+		await swapToWeth();
+	}
+
+	const showNotification = (msg, type = "info") => {
+		setNotification({ msg, type });
+	};
+
+	async function swapToWeth() {
+		try {
+			setSwapLoading(true);
+			await switchChain();
+			const web3 = new Web3(window.ethereum);
+			const tokenContract = new web3.eth.Contract(ERC20_ABI, SEPOLIA_WETH_CA);
+			const tx = await tokenContract.methods
+				.deposit()
+				.send({ from: address, value: Web3.utils.toWei("0.001", "ether") });
+			setSwapLoading(false);
+			showNotification("Swap completed 🥳", "success");
+			return tx;
+		} catch (error) {
+			console.log(error);
+			setSwapLoading(false);
+		}
 	}
 
 	useEffect(() => {
@@ -26,9 +63,23 @@ const WalletBadge = () => {
 				}
 			}}
 		>
+			{notification && (
+				<Notification
+					message={notification.msg}
+					type={notification.type}
+					onClose={() => setNotification(null)}
+				/>
+			)}
 			<img src="./logo.png" alt="Logo" className="site-logo" />
+			<p className="faucet" onClick={handleSwap}>
+				{swapLoading ? <Loader /> : "Swap 0.001ETH to WETH"}
+			</p>
 			<div className="wallet-badge">
-				<span className="wallet-text">{address ?? "Connect"}</span>
+				<span className="wallet-text">
+					{address
+						? `${address.slice(0, 4)}...${address.slice(-3)}`
+						: "Connect"}
+				</span>
 				<div className="eth-icon">
 					<FaEthereum />
 				</div>
